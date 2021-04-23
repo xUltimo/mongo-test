66644c85ca166f67cdcd8cd9aa932368f50477f1
Manuel <manuel.karner@edu.fh-joanneum.at>
2021-04-23 13:24:22 +0200

    breed & taillengthm added

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to the awesome best every made db!!!! 2"});
+    res.json({"message": "Welcome to the updated and better cat-db (breed including TIGER & taillength)"});
 });
 
 require('./app/routes/cat.routes.js')(app);
