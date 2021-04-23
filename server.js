a2be8b7630758fe3791b94c3ca049ea7c391f69b
xUltimo <manuelk4rner2@gmail.com>
2021-04-23 12:24:59 +0200

    changed welcome message

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to the awesome best every made db."});
+    res.json({"message": "Welcome to the awesome best every made db!!!!"});
 });
 
 require('./app/routes/cat.routes.js')(app);
